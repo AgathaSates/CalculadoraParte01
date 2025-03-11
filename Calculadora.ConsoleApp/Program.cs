13db0306be7f25f9cc75e42f1e24f3dd795f45e4
Agatha Sates <satesagatha@gmail.com>
2025-03-11 02:16:37 -0300

    Adicionado histórico de operações

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            int operacoes = 0;
+            string[] historico = new string[20];
+
             while (true)
             {
                 Console.Clear();
@@ -15,6 +18,7 @@
                 Console.WriteLine("3 - Multiplicação");
                 Console.WriteLine("4 - Divisão");
                 Console.WriteLine("5 - Tabuada");
+                Console.WriteLine("6 - Histórico de operações");
                 Console.WriteLine("S - Sair");
                 Console.WriteLine("-----------------------------");
                 Console.Write("Escolha uma opção: ");
@@ -26,22 +30,49 @@
 
                 else if (opcao == "5")
                 {
-                    Console.WriteLine("-----------------------------");
-                    Console.WriteLine("Tabuada");
-                    Console.WriteLine("-----------------------------");
-
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("-----------Tabuada-----------");
                     Console.Write("Digite um número: ");
                     int numeroTabuada = Convert.ToInt32(Console.ReadLine());
-
-                    for (int contador = 1; contador <= 10; contador++) 
+                    Console.WriteLine("-----------------------------");
+                    for (int contador = 1; contador <= 10; contador++)
                     {
                         int resultadoTabuada = numeroTabuada * contador;
                         Console.WriteLine($"{numeroTabuada} X {contador} = {resultadoTabuada}");
                     }
+                    Console.WriteLine("-----------------------------");
+                    Console.Write("Pressione qualquer tecla para sair: ");
                     Console.ReadLine();
                     continue;
                 }
-                
+
+                else if (opcao == "6")
+                {
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("-----Histórico de operações-----");
+                    Console.WriteLine();
+                    if (historico[0] == null)
+                    {                      
+                        Console.WriteLine("Não há operações registradas.");
+                    }
+                    else
+                    {
+                        foreach (string operacao in historico)
+                        {
+                            if (operacao != null)
+                            {
+                                Console.WriteLine(operacao);
+                            }
+                        }                       
+                    }
+                    Console.WriteLine();
+                    Console.Write("Pressione qualquer tecla para sair: ");
+                    Console.ReadLine();
+                    continue;
+                }
+
                 Console.WriteLine("-----------------------------");
                 Console.Write("Digite o primeiro número: ");
                 double primeiroNumero = Convert.ToDouble(Console.ReadLine());
@@ -51,33 +82,41 @@
                 Console.WriteLine("-----------------------------");
 
                 double resultado = 0;
+                string sinal = "";
 
                 if (opcao == "1")
-
+                {
                     resultado = primeiroNumero + segundoNumero;
+                    sinal = "+";
+                }
 
                 else if (opcao == "2")
-
+                {
                     resultado = primeiroNumero - segundoNumero;
+                    sinal = "-";
+                }
 
                 else if (opcao == "3")
-
+                {
                     resultado = primeiroNumero * segundoNumero;
+                    sinal = "x";
+                }
 
                 else if (opcao == "4")
                 {
-                    while(segundoNumero == 0)
+                    while (segundoNumero == 0)
                     {
-                        Console.WriteLine("Não é possivel dividir um número por zero");
+                        Console.Write("Não é possivel dividir um número por zero, digite um número novamente:");
                         segundoNumero = Convert.ToDouble(Console.ReadLine());
                     }
                     resultado = primeiroNumero / segundoNumero;
+                    sinal = "÷";
                 }
 
-                Console.WriteLine("-----------------------------");
                 Console.WriteLine("Resultado: " + resultado.ToString("F2"));
                 Console.WriteLine("-----------------------------");
-
+                historico[operacoes] = $"{primeiroNumero} {sinal} {segundoNumero} = {resultado.ToString("F2")}";
+                operacoes++;
                 Console.Write("Deseja continuar? (S/N): ");
                 string continuar = Console.ReadLine().ToUpper();
                 if (continuar == "N")
@@ -86,3 +125,4 @@
         }
     }
 }
+
